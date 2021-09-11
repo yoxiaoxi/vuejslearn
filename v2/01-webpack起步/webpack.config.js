cdd79f83384955f5097ba81857a002a4bf00dbe0
颜成有 <875257500@qq.com>
2021-09-11 20:39:23 +0800

    no message

--- v2/01-webpack起步/webpack.config.js
+++ v2/01-webpack起步/webpack.config.js
@@ -4,7 +4,8 @@ module.exports={
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: 'dist/'
     },
     module: {
         rules: [
@@ -24,6 +25,18 @@ module.exports={
                         noIeCompat: true
                     }
                 }]
+            },
+            {
+                test: /\.(png|jpg|gif|jpeg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: 'img/[name].[hash:8].[ext]'
+                        }
+                    }
+                ]
             }
         ]
     }
